2e087307e9a480dd75893d10a553d2b14b3fcfeb
Thaís Alcantara <tatie.alcantara@gmail.com>
2026-05-27 19:40:57 -0300

    start

---
+++ src/main/java/com/thais/terrrarium_api/TerrrariumApiApplication.java
@@ -0,0 +1,13 @@
+package com.thais.terrrarium_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerrrariumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerrrariumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thais/terrrarium_api/TerrrariumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thais.terrrarium_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerrrariumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
